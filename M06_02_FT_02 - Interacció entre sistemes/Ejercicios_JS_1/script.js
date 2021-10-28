04f9f2cc2e18e84531a74d18a4c8fe9b835afe66
Borja Garcia <bgarciab2021@cepnet.net>
2021-10-28 11:53:19 +0200

    dos ejercicios  Interacció entre sistemes

---
+++ M06_02_FT_02 - Interacció entre sistemes/Ejercicios_JS_1/script.js
@@ -0,0 +1,11 @@
+//ejercicio con alert
+
+function printarMensaje() {
+
+
+    nombre = document.getElementById("cajaTexto").value;//Recoger valor de un input text  
+    document.write("Hola " + nombre)
+
+}
+
+
---
+++ M06_02_FT_02 - Interacció entre sistemes/Ejercicios_JS_2/script.js
@@ -0,0 +1,7 @@
+function Calcular() {
+    var vr1 = document.getElementById('cajaTexto').value;
+    var vr2 = document.getElementById('cajaTexto2').value;
+
+    var promedio = (parseFloat(vr1) + parseFloat(vr2)) / 2;
+    document.getElementById('promedio').innerHTML = promedio;
+}
