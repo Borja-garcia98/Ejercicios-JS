e50008febaca16a29b6598f4981ec02c28d26af9
Borja Garcia <bgarciab2021@cepnet.net>
2021-10-28 13:09:07 +0200

    parte 3 Interacció entre sistemes acabada

---
+++ M06_02_FT_02 - Interacció entre sistemes/Ejercicios_JS_6/script.js
@@ -0,0 +1,11 @@
+
+function printarMensaje() {
+
+
+    nombre = console.log(document.getElementById("cajaTexto").value);//Recoger valor de un input text  
+   
+   
+   
+    document.write("Hola " + nombre)
+
+}
