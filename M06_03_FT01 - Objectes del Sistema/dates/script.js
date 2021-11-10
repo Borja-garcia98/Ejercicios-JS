628b35d824eb7fbd09b861e6e157e86046c20978
Borja Garcia <bgarciab2021@cepnet.net>
2021-11-10 09:46:56 +0100

    dates

--- M06_03_FT01 - Objectes del Sistema/dates/script.js
+++ M06_03_FT01 - Objectes del Sistema/dates/script.js
@@ -19,7 +19,7 @@ dia.setFullYear(2021, 10, 23);
 if (dia > hoy) {
   text = "Estamos antes del 23 de Octubre del 2021.";
 } else {
-  text = "Estamos despues del 23 de Octubre del 2021.";
+  text = "Estamos despues del 23 de Octubre del 2021";
 }
 
 document.getElementById("comparar").innerHTML = text;
